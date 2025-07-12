4af0987dd294344c35700b561993e6520a5ff35b
bby <Aleksandr.Bobyrev@stoneridge.com>
2025-07-12 23:20:40 +0300

    Fix main function return type and enhance print_array function with error handling

--- stats.c
+++ stats.c
@@ -29,8 +29,9 @@
 
 /* Size of the Data Set */
 #define SIZE (40)
+#define MAX_ELEMENTS_PER_LINE (15)
 
-void main() {
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -39,8 +40,35 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
+  /* Statistics and Printing Functions Go Here */
+  print_array(test, SIZE);
+
+  print_array(NULL, SIZE); // Test with NULL array
+  print_array(test, 0);    // Test with size 0
+
+  printf("Done\n");
+  return 0;
 }
 
 /* Add other Implementation File Code Here */
+void print_array(unsigned char *array, unsigned int size) {
+  if (array == NULL || size == 0) {
+    printf("Error: Array is empty or NULL\n");
+    return;
+  }
+
+  printf("Array: [");
+  for(unsigned int i = 0; i < size; i++) {
+    printf("%4d", array[i]);
+    if (i < size - 1) {
+      printf(",");
+      if ((i + 1) % MAX_ELEMENTS_PER_LINE == 0) {
+        printf("\n         ");
+      } else {
+        printf(" ");
+      }
+    }
+  }
+  printf("]\n");
+}
